5ea8121713c31bce7929143f68b92f50fae47677
Alex Bainbridge <abainbri@andrew.cmu.edu>
2018-03-05 22:02:59 +0000

    fixed home controller routes

--- LendingLibrary/config/routes.rb
+++ LendingLibrary/config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   # get 'rental_form' => 'reservation#rental_form', as: :rental_form
 
-  get 'home' => 'home#home', as: :home
+  get '/' => 'home#home', as: :home
   get 'about' => 'home#about', as: :about
   get 'contact' => 'home#contact', as: :contact
   get 'privacy' => 'home#privacy', as: :privacy
