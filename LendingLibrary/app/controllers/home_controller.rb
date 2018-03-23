095f976d85c093de06097bf1f6989b00ff7d17ed
Walter Donnellan <Walt@Walters-MacBook-Pro.local>
2018-03-23 17:37:44 -0400

    finished creating upload user.csv ability

--- LendingLibrary/app/controllers/home_controller.rb
+++ LendingLibrary/app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 require 'csv' 
+require 'tempfile'
+
+
 class HomeController < ApplicationController
   layout 'home'
 
@@ -15,14 +18,31 @@ class HomeController < ApplicationController
   end
 
   def create_users
-  	# byebug
-  	csv_text = File.read(params[:users_csv])
-	csv = CSV.parse(csv_text, :headers => true)
+  	failed_emails = Array.new
+  	file = params['create_users']['users_csv'].tempfile
+	csv = CSV.read(file, :headers => true)
 	csv.each do |row|
-		console.log(row)
-  		# User.create!(row.to_hash)
+		@user = User.new
+		@user.email = row['email']
+		@user.first_name = row['first_name']
+		@user.last_name = row['last_name']
+		@user.password = row['password']
+		@user.password_confirmation = row['password']
+		@user.phone_num = row['phone_num']
+		@user.class_size = row['class_size']
+		@user.school_id = row['school_id']
+		@user.is_active = true
+		@user.role = params['create_users']['role']
+		unless @user.save
+			failed_emails.push(row['email'])
+		end
 	end
-	redirect_to users_url
+	if failed_emails.size > 0
+		data = "All users added except for users with the following emails: "+failed_emails.join(", ")
+		send_data( data, :filename => "failed_accounts.txt" )
+	else
+		redirect_to users_url
+	end
   end
 
   def privacy
