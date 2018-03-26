d7e9d538388f00c107f5588d14306366a32349ea
Killian Huang <xhuang1@andrew.cmu.edu>
2018-03-26 10:38:43 -0400

    correct security issues

--- LendingLibrary/app/controllers/reservations_controller.rb
+++ LendingLibrary/app/controllers/reservations_controller.rb
@@ -31,40 +31,19 @@ class ReservationsController < ApplicationController
      @today_pickup = Reservation.picking_up_today
   end
 
+  # POST /select_dates
   def select_dates
     @item_category = ItemCategory.find(params[:item_category])
-
-
-    # @prep = params[:reservation]
-    # @prep2 = JSON.parse @prep.gsub('=>', ':')
-    #
-    # puts @prep2.to_s + "\n"
-    # puts @prep2.to_h.to_s
-
-    # @reservation = Reservation.new(@prep2)
-    # @reservation = Reservation.new(@prep2.to_h)
-    # puts " ============== ============== ============== ============== "
-
-        # params.each do |key,value|
-        #   puts "Param #{key}: #{value}"
-        # end
-
     @pick_up_date = params[:reservation_select_dates][:pick_up_date]
     @return_date = params[:reservation_select_dates][:return_date]
 
-    # puts " ============== ============== ============== ============== "
-    # puts @reservation.attributes
+    #FIXME: add more validations on dates
+    if @pick_up_date.to_s.length < 1 || @return_date.to_s.length < 1
+      redirect_to new_reservation_path(:step => 2, :item_category => @item_category), notice: 'Dates cannot be null.'
 
-    # respond_to do |format|
-      # if @reservation.save
-    redirect_to new_reservation_path(:step => 3, :pick_up_date => @pick_up_date, :return_date => @return_date, :item_category => @item_category)
-        # format.json { render "reservation_confirmation", status: :created, location: @reservation }
-      # else
-        # format.html { render :new }
-        # format.json { render json: @reservation.errors, status: :unprocessable_entity }
-      # end
-    # end
-
+    else
+       redirect_to new_reservation_path(:step => 3, :pick_up_date => @pick_up_date, :return_date => @return_date, :item_category => @item_category)
+    end
   end
 
 
@@ -75,53 +54,29 @@ class ReservationsController < ApplicationController
 
   # GET /reservations/new
   def new
-    # puts " ******************* IN NEW *******************"
-    # params.each do |key,value|
-    #   puts "Param #{key}: #{value}"
-    # end
-
     @step = params[:step]
 
     # FIXME
     @user = User.find(3)
 
-    # puts "------- got user:" + @user.first_name
     @item_category = ItemCategory.find(params[:item_category])
 
+    @reservation = Reservation.new
+    # sample a random item of the picked item category
+    @item = @item_category.items.sample(1).first
+    # get available kits for this particular item
+    @kits = Kit.available_kits
+    # generate a random kit based on available kits
+    offset2 = rand(@kits.count)
+    @kit = @kits.at(offset2)
 
-        @reservation = Reservation.new
-        # sample a random item of the picked item category
-        @item = @item_category.items.sample(1).first
-        # get available kits for this particular item
-        @kits = Kit.available_kits
-        # generate a random kit based on available kits
-        offset2 = rand(@kits.count)
-        puts "kits size: " + @kits.count.to_s
-        @kit = @kits.at(offset2)
+    @reservation.kit_id = @kit.id
+    @reservation.teacher_id = @user.id
 
-        @reservation.kit_id = @kit.id
-        # new_reservation_path(:step => 3, :item_category => @item_category.id)
-
-
-
-        @reservation.teacher_id = @user.id
-
-        unless params['pick_up_date'].nil?
-          @reservation.pick_up_date = Date::strptime((params['pick_up_date'].to_s), "%m/%d/%Y")
-          @reservation.return_date = Date::strptime((params['return_date'].to_s), "%m/%d/%Y")
-        end
-
-          # puts " ============== ============== ============== ============== "
-          #   puts "param 1: " + params['pick_up_date'].to_s
-          #   puts "param 2: " + params['return_date'].to_s
-          #   puts "pick up date: " + @reservation.pick_up_date.to_s
-          #   puts "return date: " + @reservation.return_date.to_s
-          #
-          #
-          #   puts " ============== ============== ============== ============== "
-          #   puts "step 3 \n"
-          #   puts @reservation.attributes
-
+    unless params['pick_up_date'].nil? || params['return_date'].nil?
+      @reservation.pick_up_date = Date::strptime((params['pick_up_date'].to_s), "%m/%d/%Y")
+      @reservation.return_date = Date::strptime((params['return_date'].to_s), "%m/%d/%Y")
+    end
 
   end
 
