09802494d783f5c6c2e29764728a64b5668196d4
Killian Huang <xhuang1@andrew.cmu.edu>
2018-03-26 23:28:33 -0400

    added documentation, redirecting to rental history page, change popup notice

--- LendingLibrary/app/controllers/reservations_controller.rb
+++ LendingLibrary/app/controllers/reservations_controller.rb
@@ -34,45 +34,48 @@ class ReservationsController < ApplicationController
   # POST /select_dates
   def select_dates
     @item_category = ItemCategory.find(params[:item_category])
+    # FIXME: hypothetical date restriction
+    @start_date = Date.today.beginning_of_month.next_month
+    @end_date = Date.today.end_of_month.next_month
+
     @pick_up_date = (params[:reservation_select_dates][:pick_up_date]).to_date
     @return_date = (params[:reservation_select_dates][:return_date]).to_date
 
-    #FIXME: add more validations on dates
-
-    @start_date = Date.today.beginning_of_month.next_month
-    @end_date = Date.today.end_of_month.next_month
 
     if @pick_up_date.nil?
       redirect_to new_reservation_path(:step => 2, :item_category => @item_category), notice: 'Pick up date cannot be null.'
     elsif @return_date.nil?
-
       redirect_to new_reservation_path(:step => 2, :item_category => @item_category), notice: 'Return date cannot be null.'
     elsif @pick_up_date < @start_date
       redirect_to new_reservation_path(:step => 2, :item_category => @item_category), notice: 'Pick up date must be after start date.'
     elsif @return_date < @pick_up_date
-      redirect_to new_reservation_path(:step => 2, :item_category => @item_category), notice: 'Return date must be after  pick update.'
+      redirect_to new_reservation_path(:step => 2, :item_category => @item_category), notice: 'Return date must be after pick update.'
     else
-       redirect_to new_reservation_path(:step => 3, :pick_up_date => @pick_up_date, :return_date => @return_date, :item_category => @item_category)
+      redirect_to new_reservation_path(:step => 3, :pick_up_date => @pick_up_date, :return_date => @return_date, :item_category => @item_category)
     end
+
   end
 
 
   # GET /reservations/1
   # GET /reservations/1.json
   def show
+    # FIXME current user
+    @user = User.find(7)
+    @reservations = Reservation.select{|res| res.teacher_id == @user.id}
   end
 
   # GET /reservations/new
   def new
     @step = params[:step]
 
-    # FIXME
+    # FIXME current user
     @user = User.find(7)
     @reservation = Reservation.new
 
-    params.each do |key, value|
-      "#{key}: #{value}"
-    end
+    # params.each do |key, value|
+    #   "#{key}: #{value}"
+    # end
 
     unless @step.nil?
       @item_category = ItemCategory.find(params[:item_category])
@@ -89,9 +92,9 @@ class ReservationsController < ApplicationController
     end
 
     unless params['pick_up_date'].nil? || params['return_date'].nil?
-      # FIXME
+      # FIXME release form id
       @reservation.release_form_id = 1
-
+      # FIXME: hypothetical date restriction
       @reservation.start_date = Date.today.beginning_of_month.next_month
       @reservation.end_date = Date.today.end_of_month.next_month
       @reservation.pick_up_date = params['pick_up_date']
@@ -111,7 +114,7 @@ class ReservationsController < ApplicationController
 
     respond_to do |format|
       if @reservation.save
-        format.html { redirect_to @reservation, notice: 'Reservation was successfully created.' }
+        format.html { redirect_to @reservation, notice: 'Thank you for supporting the STEAM Kit rental program.' }
         format.json { render :show, status: :created, location: @reservation }
       else
         format.html { render :new }
--- LendingLibrary/app/controllers/users_controller.rb
+++ LendingLibrary/app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:user])
+    @reservations = Reservation.select{|res| res.teacher_id == @user.id}
   end
 
   # GET /users/new
