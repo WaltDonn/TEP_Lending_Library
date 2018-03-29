58bb93442521a51c86624e5d92a257bbf8efa3d2
Killian Huang <xhuang1@andrew.cmu.edu>
2018-03-28 20:51:35 -0400

    added rental history

--- LendingLibrary/app/controllers/users_controller.rb
+++ LendingLibrary/app/controllers/users_controller.rb
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/rental_history
   def rental_history
+    @reservations = Reservation.select{|res| res.teacher_id == @user.id}
   end
 
   def reservation_user_edit
