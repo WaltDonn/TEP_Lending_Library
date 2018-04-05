136c30711629895ff3b593a38fc1c896d1456b88
Alex Bainbridge <abainbri@andrew.cmu.edu>
2018-04-05 13:16:32 +0000

    item edit page mostly done. need to add dynamic components

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,6 +17,7 @@ class Item < ApplicationRecord
     #Relationships
     belongs_to :kit, optional: true
     belongs_to :item_category
+    accepts_nested_attributes_for :item_category
 
     def item_component_condition
         if(self.condition == "Good")
