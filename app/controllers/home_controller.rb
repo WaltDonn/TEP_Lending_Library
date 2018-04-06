66bae9c9bcb90c34378c63dad2039081946b3c1c
Killian Huang <xhuang1@andrew.cmu.edu>
2018-04-06 09:56:00 -0400

    lookup school by name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
-require 'csv' 
+require 'csv'
 require 'tempfile'
 
 class HomeController < ApplicationController
@@ -13,27 +13,27 @@ class HomeController < ApplicationController
 
   def contact
   end
-  
+
   def privacy
   end
-  
+
   def reports
   end
 
   def upload_users
   	authorize! :upload_users, nil
   end
-  
+
 
   def create_users
   	authorize! :create_users, nil
- 
+
   	failed_emails = Array.new
   	file = params['create_users']['users_csv'].tempfile
 		csv = CSV.read(file, :headers => true)
 		csv.each do |row|
 			generated_password = Devise.friendly_token.first(8)
-			
+
 			@user = User.new
 			@user.email = row['email']
 			@user.first_name = row['first_name']
@@ -42,16 +42,16 @@ class HomeController < ApplicationController
 			@user.password_confirmation = generated_password
 			@user.phone_num = row['phone_num']
 			@user.class_size = row['class_size']
-			@user.school_id = row['school_id']
+			@user.school_id = School.by_name(row['school']).first.id
 			@user.is_active = true
 			@user.role = params['create_users']['role']
-			unless @user.save 
+			unless @user.save
 				unless @user.errors.messages[:email] == ["has already been taken"]
 					failed_emails.push(row['email'])
 				end
 			end
 		end
-		
+
 		if failed_emails.size > 0
 			data = "All users added except for users with the following emails: "+failed_emails.join(", ")
 			send_data( data, :filename => "failed_accounts.txt" )
@@ -59,15 +59,15 @@ class HomeController < ApplicationController
 			redirect_to users_url
 		end
   end
-  
+
   def upload_schools
   	authorize! :upload_schools, nil
   end
-  
+
 
   def create_schools
   	authorize! :create_schools, nil
- 
+
   	failed_schools = Array.new
   	file = params['create_schools']['schools_csv'].tempfile
 		csv = CSV.read(file, :headers => true)
@@ -80,13 +80,13 @@ class HomeController < ApplicationController
 			@school.state = row['state']
 			@school.zip = row['zip']
 			@school.is_active = true
-			unless @school.save 
+			unless @school.save
 				unless @school.errors.messages[:name] == ["already exists for this school at this location"]
 					failed_schools.push(row['name'])
 				end
 			end
 		end
-		
+
 		if failed_schools.size > 0
 			data = "All schools added except for schools with the following names: "+failed_schools.join(", ")
 			send_data( data, :filename => "failed_schools.txt" )
@@ -118,5 +118,5 @@ class HomeController < ApplicationController
 	# #probs not where i should be putting it, temp for now, should also make them name the pdf or it be generated
 	# kit.to_file('app/views/reports/hello2.pdf')
   end
-  
+
 end
