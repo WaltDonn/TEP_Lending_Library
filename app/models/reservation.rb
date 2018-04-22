e8e1ceae80fac0f4edb5ad3d55d9ed0c02210d7b
Walter Donnellan <Walt@Walters-MacBook-Pro.local>
2018-04-22 16:15:39 -0400

    date fix

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -33,7 +33,7 @@ class Reservation < ApplicationRecord
 
     scope :get_month,             ->(month){where("EXTRACT(MONTH FROM pick_up_date) = ?", month)}
     scope :returning_today,       -> { where("picked_up = ? AND returned = ? AND return_date <= ?", true, false, Date.current)}
-    scope :picking_up_today,      -> { where("picked_up = ? AND pick_up_date >= ?", false, Date.current) }
+    scope :picking_up_today,      -> { where("picked_up = ? AND pick_up_date <= ?", false, Date.current) }
 
 
     def past_due?
